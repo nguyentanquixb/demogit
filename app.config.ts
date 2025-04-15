996c8474ac6620b9f8472024e083d8ea3cea3134
nguyentanquixb <nguyentanquixb@gmail.com>
2025-04-15 10:47:40 +0700

    add new file

---
+++ app.config.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { SaveOutline } from '@ant-design/icons-angular/icons';
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    { provide: NZ_ICONS, useValue: [SaveOutline] }
+  ]
+};
